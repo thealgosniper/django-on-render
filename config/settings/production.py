7674d77d362e1f35594dd72e2eeb7db1cbec4947
thealgosniper <87916941+thealgosniper@users.noreply.github.com>
2023-05-05 12:53:24 +0530

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -178,3 +178,9 @@ LOGGING = {
 
 # Your stuff...
 # ------------------------------------------------------------------------------
+def show_toolbar(request):
+    return True
+
+DEBUG_TOOLBAR_CONFIG = {
+  "SHOW_TOOLBAR_CALLBACK" : show_toolbar,
+}
